1306477f99d1207082db2597a1da2eaff5f4b80b
Marcel Frei <marcel.frei@electronix.ch>
2019-10-24 00:14:11 +0200

    install wasm-gc

--- ci/setup.sh
+++ ci/setup.sh
@@ -9,6 +9,13 @@ sudo apt-get install -y cmake pkg-config libssl-dev
 # Install wasm32 for nightly
 rustup target add wasm32-unknown-unknown
 
+# Install wasm-gc
+if ! [ -x "$(command -v wasm-gc)" ]; then
+    cargo install --git https://github.com/alexcrichton/wasm-gc
+else
+    echo "wasm-gc already installed"
+fi
+
 # Show rust information
 rustc --version
 rustup --version
