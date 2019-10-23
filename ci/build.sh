9786bc946f25110938644766ad92d8fab2bb4244
Marcel Frei <marcel.frei@electronix.ch>
2019-10-23 23:30:48 +0200

    use rust in travis.yml

--- ci/build.sh
+++ ci/build.sh
@@ -11,18 +11,3 @@ source ~/.cargo/env
 rustc --version
 rustup --version
 cargo --version
-
-case $TARGET in
-	"native")
-		sudo apt-get -y update
-		sudo apt-get install -y cmake pkg-config libssl-dev
-
-		cargo test --all --release --locked "$@"
-		;;
-
-	"wasm")
-		# Install prerequisites and build all wasm projects
-		./scripts/init.sh
-		./scripts/build.sh "$@"
-		;;
-esac
--- ci/setup.sh
+++ ci/setup.sh
@@ -2,10 +2,19 @@
 
 set -eux
 
+# Install packages used for substrate
+sudo apt-get -y update
+sudo apt-get install -y cmake pkg-config libssl-dev
+
 # Enable warnings about unused extern crates
 export RUSTFLAGS=" -W unused-extern-crates"
 
 # Install rustup and the specified rust toolchain.
-curl https://sh.rustup.rs -sSf | sh -s -- --default-toolchain=$RUST_TOOLCHAIN -y
+# curl https://sh.rustup.rs -sSf | sh -s -- --default-toolchain=$RUST_TOOLCHAIN -y
 
-rustup show
+# Load cargo environment. Specifically, put cargo into PATH.
+source ~/.cargo/env
+
+rustc --version
+rustup --version
+cargo --version
