b56906d1c9622bea928e504a130c5c42a2cd65e5
Marcel Frei <marcel.frei@electronix.ch>
2019-10-23 23:11:35 +0200

    initial files for travisCI

---
+++ ci/build.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+set -eux
+
+# Enable warnings about unused extern crates
+export RUSTFLAGS=" -W unused-extern-crates"
+
+# Load cargo environment. Specifically, put cargo into PATH.
+source ~/.cargo/env
+
+rustc --version
+rustup --version
+cargo --version
+
+case $TARGET in
+	"native")
+		sudo apt-get -y update
+		sudo apt-get install -y cmake pkg-config libssl-dev
+
+		cargo test --all --release --locked "$@"
+		;;
+
+	"wasm")
+		# Install prerequisites and build all wasm projects
+		./scripts/init.sh
+		./scripts/build.sh "$@"
+		;;
+esac
---
+++ ci/setup.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+set -eux
+
+# Enable warnings about unused extern crates
+export RUSTFLAGS=" -W unused-extern-crates"
+
+# Install rustup and the specified rust toolchain.
+curl https://sh.rustup.rs -sSf | sh -s -- --default-toolchain=$RUST_TOOLCHAIN -y
+
+rustup show
